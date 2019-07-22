cbc139f8f899ed3b7d6b6b38746bb3024f733c6b
Tim <nebukam@gmail.com>
2019-07-22 01:08:22 -0400

    Initial commit

---
+++ Runtime/JobHandler.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Collections;
+
+namespace Nebukam.JobAssist
+{
+
+    public interface IJobHandler
+    {
+        IJobHandler jobDependency { get; }
+        JobHandle currentHandle { get; }
+        JobHandle Schedule(float delta, IJobHandler dependsOn = null);
+        void Complete();
+        
+    }
+
+
+    public abstract class JobHandler<T> : IJobHandler
+        where T : struct, IJob
+    {
+
+        protected IJobHandler m_jobDependency = null;
+        public IJobHandler jobDependency { get { return m_jobDependency; } }
+
+        protected T m_currentJob;
+        protected JobHandle m_currentHandle;
+        public JobHandle currentHandle { get { return m_currentHandle; } }
+
+        protected bool m_scheduled = false;
+        protected bool m_completed = false;
+        public bool completed {
+            get {
+                if (m_scheduled)
+                {
+                    if (m_currentHandle.IsCompleted)
+                    {
+                        Complete();
+                        return m_completed;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return m_completed;
+                }
+            }
+        }
+
+        public JobHandle Schedule(float delta, IJobHandler dependsOn = null)
+        {
+            if (m_scheduled) { return m_currentHandle; }
+            m_scheduled = true;
+            m_completed = false;
+
+            m_currentJob = new T();
+            Prepare(ref m_currentJob, delta);
+
+            if (dependsOn != null)
+            {
+                m_jobDependency = dependsOn;
+                m_currentHandle = m_currentJob.Schedule(m_jobDependency.currentHandle);
+            }
+            else
+            {
+                m_jobDependency = null;
+                m_currentHandle = m_currentJob.Schedule();
+            }
+
+            
+            return m_currentHandle;
+        }
+
+        protected abstract void Prepare(ref T job, float delta);
+
+        public void Complete()
+        {
+            if (!m_scheduled) { return; }
+
+            m_jobDependency?.Complete();
+            m_currentHandle.Complete();
+
+            m_scheduled = false;
+            m_completed = true;
+
+            Apply(ref m_currentJob);
+
+        }
+
+        protected abstract void Apply(ref T job);
+
+
+    }
+
+}
---
+++ Runtime/JobStack.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Jobs;
+
+namespace Nebukam.JobAssist
+{
+    public class JobStack : IJobHandler
+    {
+
+        protected List<IJobHandler> m_stack = new List<IJobHandler>();
+
+        //Should be null
+        protected IJobHandler m_jobDependency = null;
+        public IJobHandler jobDependency { get { return m_jobDependency; } }
+
+        //Last dependency of the stack
+        protected JobHandle m_currentHandle;
+        public JobHandle currentHandle { get { return m_currentHandle; } }
+
+        public void Add(IJobHandler job)
+        {
+            if (m_stack.Contains(job)) { return; }
+            m_stack.Add(job);
+        }
+
+        protected bool m_scheduled = false;
+        
+        public JobHandle Schedule(float delta, IJobHandler dependsOn = null)
+        {
+
+            if (m_scheduled) { return m_currentHandle; }
+            m_scheduled = true;
+            
+            m_jobDependency = dependsOn;
+
+            int count = m_stack.Count;
+            IJobHandler job, prevJob = dependsOn;
+
+            for (int i = 0; i < count; i++)
+            {
+                job = m_stack[i];
+
+                if (prevJob == null)
+                {
+                    m_currentHandle = job.Schedule(delta);
+                }
+                else
+                {
+                    m_currentHandle = job.Schedule(delta, prevJob);
+                }
+
+                prevJob = job;
+
+            }
+
+            return m_currentHandle;
+
+        }
+
+        public void Complete()
+        {
+            if (!m_scheduled) { return; }
+
+            m_jobDependency?.Complete();
+
+            int count = m_stack.Count;
+            for(int i = 0; i < count; i++)
+            {
+                m_stack[i].Complete();
+            }
+
+            m_scheduled = false;
+        }
+    }
+}
