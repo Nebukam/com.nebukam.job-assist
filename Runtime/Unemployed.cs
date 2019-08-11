0c24b1fff46b90653cd9a447d3667cf9a04c0b62
Tim <nebukam@gmail.com>
2019-08-11 00:42:43 -0400

    Updated dependencies

--- Runtime/Unemployed.cs
+++ Runtime/Unemployed.cs
@@ -1,6 +1,8 @@
-﻿using Unity.Jobs;
+﻿using Unity.Burst;
+using Unity.Jobs;
 
 namespace Nebukam.JobAssist
 {
+    [BurstCompile]
     public struct Unemployed : IJob { public void Execute() { } }
 }
