222372b78919645e6dfbfee68fd3cbdf5a952f84
Tim <nebukam@gmail.com>
2019-09-28 18:12:02 -0400

    Update ParallelProcessor.cs

--- Runtime/ParallelProcessor.cs
+++ Runtime/ParallelProcessor.cs
@@ -71,7 +71,7 @@ namespace Nebukam.JobAssist
             m_scheduled = true;
             m_hasJobHandleDependency = false;
 
-            m_currentJob = new T();
+            m_currentJob = default;
 
             Lock();
             int jobLength = Prepare(ref m_currentJob, m_deltaSum * deltaMultiplier);
@@ -116,7 +116,7 @@ namespace Nebukam.JobAssist
             m_hasJobHandleDependency = true;
             m_procDependency = null;
 
-            m_currentJob = new T();
+            m_currentJob = default;
 
             Lock();
             int jobLength = Prepare(ref m_currentJob, m_deltaSum * deltaMultiplier);
